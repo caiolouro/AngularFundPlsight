cae89647943bb12d9fdff608768f809ecb34b249
caiolouro <caiolouro@gmail.com>
2015-11-17 10:10:17 -0200

    More comments on the web server script file

--- DemoApp/scripts/web-server.js
+++ DemoApp/scripts/web-server.js
@@ -3,8 +3,10 @@ var express = require('express'),
 
 var app = express();
 
+//__dirname é a pasta atual
 var rootPath = path.normalize(__dirname + '/../');
 
+//.static não interpreta as páginas, apenas as serve de forma estática
 app.use(express.static(rootPath + '/app'));
 app.listen(8000);
 console.log('Listening on port 8000...');
